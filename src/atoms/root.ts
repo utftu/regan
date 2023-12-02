0c3999cebc54a6ec8213487aac0590bfa0aeaefa
utftufutukgyftryidytftuv <overby999@gmail.com>
2023-12-02 14:15:35 +0300

    02.12.23 14:15

--- src/atoms/root.ts
+++ src/atoms/root.ts
@@ -3,18 +3,32 @@ import {createControlledPromise, PromiseControls} from 'utftu';
 
 class InternalAtomConfig {
   shards: TxShard[] = [];
-  omittedShard: TxShard[] = [];
+  omittedShards: TxShard[] = [];
 }
 
+const OMITTED_LIMIT = 10;
+
+type Changes = Map<Atom, any>;
+type Exec = () => Promise<() => void>;
+
+const TxStatuses = {
+  init: 0,
+  running: 1,
+  finished: 2,
+  closed: 3,
+};
+
 class Tx {
-  changes: Map<Atom, any> = new Map();
+  // changes: Map<Atom, any> = new Map();
+  exec: Exec;
+  execReault?: () => void;
   shards: TxShard[] = [];
   root: Root;
   promise: Promise<any>;
   promiseControls: PromiseControls<any>;
 
-  constructor(changes: Map<Atom, any>, root: Root) {
-    this.changes = changes;
+  constructor(exec: Exec, root: Root) {
+    this.exec = exec;
     this.root = root;
     const [promise, promiseControls] = createControlledPromise<any>();
     this.promise = promise;
@@ -25,6 +39,9 @@ class Tx {
 
   checkOmmit() {
     for (const shard of this.shards) {
+      if (shard.internalAtomConfig.omittedShards.length > OMITTED_LIMIT) {
+        return false;
+      }
       const atomConfigShards = shard.internalAtomConfig.shards;
 
       const position = atomConfigShards.indexOf(shard);
@@ -36,17 +53,41 @@ class Tx {
     return true;
   }
 
-  // async exec() {
-  //   const shardResults = await Promise.all(
-  //     this.shards.map((shard) => shard.exec())
-  //   );
+  omit() {
+    const positions = [];
+    for (const shard of this.shards) {
+      const atomConfigShards = shard.internalAtomConfig.shards;
 
-  //   for (const syncActions of shardResults) {
-  //     for (const syncAction of syncActions) {
-  //       syncAction();
-  //     }
-  //   }
-  // }
+      const position = atomConfigShards.indexOf(shard);
+      positions.push(position);
+
+      atomConfigShards.splice(position, 1);
+      shard.internalAtomConfig.omittedShards.push(shard);
+    }
+
+    for (let i = 0; i <= this.shards.length; i++) {
+      const shard = this.shards[i];
+
+      const atomConfigShards = shard.internalAtomConfig.shards;
+      const nextShard = atomConfigShards[positions[i]];
+
+      if (nextShard) {
+        nextShard.tx.root.handleTx(nextShard.tx);
+      }
+    }
+  }
+
+  checkReadyRun() {
+    for (const shard of this.shards) {
+      const atomConfigShards = shard.internalAtomConfig.shards;
+
+      if (atomConfigShards[0] !== shard) {
+        return false;
+      }
+    }
+
+    return true;
+  }
 }
 
 class TxShard {
@@ -58,48 +99,12 @@ class TxShard {
 
   tx: Tx;
   internalAtomConfig: InternalAtomConfig;
-  position: number;
-  // internalAtom: Atom;
-  // value: any;
 
-  constructor(
-    tx: Tx,
-    internalAtomConfig: InternalAtomConfig
-    // value: any
-  ) {
+  constructor(tx: Tx, internalAtomConfig: InternalAtomConfig) {
     this.tx = tx;
     tx.shards.push(this);
     this.internalAtomConfig = internalAtomConfig;
-    // this.value = value;
   }
-
-  // async exec() {
-  //   const funcs: (() => void)[] = [];
-  //   await this.internalAtom.update({
-  //     data: {
-  //       syncActions: funcs,
-  //     },
-  //   });
-  //   return funcs;
-  // }
-
-  // check() {
-  //   // todo check not exist
-
-  //   if (!this.tx.root.internalAtoms.has(this.internalAtom)) {
-  //     return true;
-  //   }
-
-  //   const internalAtomConfig = this.tx.root.internalAtoms.get(
-  //     this.internalAtom
-  //   )!;
-
-  //   if (internalAtomConfig.transactions.length === 0) {
-  //     return true;
-  //   }
-
-  //   // if (ex)
-  // }
 }
 
 class Links {
@@ -131,33 +136,10 @@ class Links {
 
 class Root {
   links = new Links();
-  // links: Map<Atom, Atom[]> = new Map();
   internalAtoms: Map<Atom, InternalAtomConfig> = new Map();
 
-  addLink(externalAtom: Atom, internalAtom: Atom) {
-    if (!this.links.has(externalAtom)) {
-      this.links.set(externalAtom, [internalAtom]);
-    } else {
-      this.links.get(externalAtom)!.push(externalAtom);
-    }
-  }
-
-  removeLink(externalAtom: Atom, internalAtom: Atom) {
-    if (!this.links.has(externalAtom)) {
-      return;
-    }
-    const newInternalAtoms = this.links
-      .get(externalAtom)!
-      .filter((localInternalAtom) => internalAtom !== localInternalAtom);
-
-    if (newInternalAtoms.length === 0) {
-      this.links.delete(externalAtom);
-    } else {
-      this.links.set(externalAtom, newInternalAtoms);
-    }
-  }
-
-  checkTx(tx: Tx) {
+  // not check prev
+  handleTx(tx: Tx) {
     // 1
     if (tx.status === 'running') {
       return;
@@ -165,14 +147,31 @@ class Root {
 
     // 2
     if (tx.status === 'finished') {
+      // гарантировано что вырезанная транзакция будет первой
+      tx.status = 'closed';
       tx.shards.forEach((shard) => {
         shard.internalAtomConfig.shards.splice(0, 1);
       });
       tx.shards.forEach((shard) => {
         const txs = shard.internalAtomConfig.shards;
         if (txs.length > 0) {
-          this.checkTx(txs[0].tx);
+          this.handleTx(txs[0].tx);
         }
+      });
+      return;
+    }
+
+    if (tx.checkOmmit()) {
+      tx.omit();
+      return;
+    }
+
+    if (tx.checkReadyRun()) {
+      tx.status = 'running';
+      tx.exec().then((result) => {
+        tx.execReault = result;
+        tx.status = 'finished';
+        tx.root.handleTx(tx);
       });
     }
 
@@ -182,78 +181,73 @@ class Root {
         const internalAtomConfig = shard.internalAtomConfig;
         const atomConfigShards = internalAtomConfig.shards;
 
+        const position = tx.shards.indexOf(shard);
+
+        // if ()
+
         for (let j = 0; j < atomConfigShards.length; j++) {
           const atomConfigShard = atomConfigShards[j];
 
-          // if ()
-        }
+          if (atomConfigShard === shard) {
+          }
 
-        // if (shard.)
+          const canOmit = atomConfigShard.tx.checkOmmit();
 
-        // for (co)
-
-        // if (tsx.at(-1) === shard) {
-        //   continue;
-        // }
-
-        // if (0) {
-
-        // }
-      }
-      // 3
-      for (const shard of tx.shards) {
-        const tsx = shard.internalAtomConfig.shards;
-        if (tsx[tsx.length - 1] === shard) {
-          continue;
-        }
-
-        // const position
-
-        if (tsx[tsx.length - 1] !== shard) {
-          if (shard.internalAtomConfig.shards.length !== 1) {
-            return;
+          if (canOmit) {
+            atomConfigShard.tx.omit();
           }
         }
       }
-    }
+      // // 3
+      // for (const shard of tx.shards) {
+      //   const tsx = shard.internalAtomConfig.shards;
 
-    for (const shard of tx.shards) {
+      //   if (tsx[tsx.length - 1] === shard) {
+      //     continue;
+      //   }
+
+      //   if (tsx[tsx.length - 1] !== shard) {
+      //     if (shard.internalAtomConfig.shards.length !== 1) {
+      //       return;
+      //     }
+      //   }
+      // }
     }
   }
 
-  addTx(tx: Tx) {
-    let emptyAtoms = true;
+  // addTx(tx: Tx) {
+  //   let emptyAtoms = true;
 
-    for (const [externalAtom, value] of tx.changes.entries()) {
-      if (!this.links.has(externalAtom)) {
-        return;
-      }
+  //   for (const [externalAtom, value] of tx.changes.entries()) {
+  //     if (!this.links.has(externalAtom)) {
+  //       return;
+  //     }
 
-      const internalAtoms = this.links.get(externalAtom) ?? [];
+  //     const internalAtoms = this.links.get(externalAtom) ?? [];
 
-      for (const internalAtom of internalAtoms) {
-        // check exist
-        let internalAtomConfig: InternalAtomConfig;
-        if (!this.internalAtoms.has(internalAtom)) {
-          internalAtomConfig = new InternalAtomConfig();
-          this.internalAtoms.set(internalAtom, internalAtomConfig);
-        } else {
-          internalAtomConfig = this.internalAtoms.get(internalAtom)!;
-        }
+  //     for (const internalAtom of internalAtoms) {
+  //       // check exist
+  //       let internalAtomConfig: InternalAtomConfig;
+  //       if (!this.internalAtoms.has(internalAtom)) {
+  //         internalAtomConfig = new InternalAtomConfig();
+  //         this.internalAtoms.set(internalAtom, internalAtomConfig);
+  //       } else {
+  //         internalAtomConfig = this.internalAtoms.get(internalAtom)!;
+  //       }
 
-        if (internalAtomConfig.transactions.length !== 0) {
-          emptyAtoms = false;
-        }
+  //       if (internalAtomConfig.transactions.length !== 0) {
+  //         emptyAtoms = false;
+  //       }
 
-        const position = internalAtomConfig.shards.length;
-        internalAtomConfig.transactions.push(
-          new TxShard(tx, internalAtom, position)
-        );
-      }
-    }
+  //       const position = internalAtomConfig.shards.length;
+  //       internalAtomConfig.transactions.push(
+  //         new TxShard(tx, internalAtom, position)
+  //       );
+  //     }
+  //   }
 
-    //   if (emptyAtoms === true) {
-    //   }
-    // }
-  }
+  //   //   if (emptyAtoms === true) {
+  //   //   }
+  //   // }
+  // }
 }
