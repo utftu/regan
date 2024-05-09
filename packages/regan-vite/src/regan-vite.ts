6b5fdee527247880e26ea4bc0a08ae512e8158f0
utftufutukgyftryidytftuv <overby999@gmail.com>
2024-05-09 21:46:08 +0300

    09.05.24 21:46

--- packages/regan-vite/src/regan-vite.ts
+++ packages/regan-vite/src/regan-vite.ts
@@ -1,3 +1,23 @@
-export const reganVite = () {
-  return {} satisfies Plugin
-}
+import {Plugin} from 'vite';
+
+export const reganVite = () => {
+  return {
+    name: 'vite-plugin-regan',
+    enforce: 'pre',
+    config() {
+      return {
+        // While this config is unconditional, it'll only be used if Babel is not
+        esbuild: {
+          jsx: 'automatic',
+          jsxDev: false,
+          jsxImportSource: 'regan',
+          jsxFragment: 'Fragment',
+          jsxFactory: 'createElement',
+        },
+        optimizeDeps: {
+          include: ['preact/jsx-runtime', 'preact/jsx-dev-runtime'],
+        },
+      };
+    },
+  } satisfies Plugin;
+};
