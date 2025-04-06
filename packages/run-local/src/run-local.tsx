49c39617ac289175b967a33f74e2cf4341d35ab1
utftufutukgyftryidytftuv <overby999@gmail.com>
2025-04-06 23:16:25 +0300

    06.04.25 23:16

--- packages/run-local/src/run-local.tsx
+++ packages/run-local/src/run-local.tsx
@@ -1,61 +1,30 @@
-import {FC, JsxNode, render} from 'regan';
 import {atom} from 'strangelove';
-// import {JsxNode as JsxNode2} from '../../regan/src/regan.ts';
-
-const a = <div>2323</div>;
-
-// const aa = null as any as JsxNode;
-// const bb = (<div>123</div>) as JSX.Element;
-// const dd: JsxNode<any, any> = bb;
+import {ErrorLogger, FC, render} from 'regan';
 
 const App = () => {
-  return <Component />;
+  return (
+    <ErrorLogger>
+      <Component />
+    </ErrorLogger>
+  );
 };
 
-const Component: FC = (_) => {
-  // const state = createAtom('hello');
-  // const savedValue = createAtom('');
-
+const Component: FC = () => {
   const name = atom('Aleksey');
-  // const family = createAtom('Malyuta');
 
-  // const MyHello = async () => {
-  //   // await fetch('234');
-  //   return 'sdsdssdsdsdsdsds';
-  // };
+  setTimeout(() => {
+    name.set('Ivan');
+  }, 1000);
 
-  // MyHello.returnLength = 'sdsdssdsdsdsdsds';
-  // const atom = createAtom(MyHello);
-
-  // setTimeout(() => {
-  //   name.set('Ivan');
-  // }, 1000);
-
-  return <div>My name is {name}</div>;
-
-  // return (
-  //   <div>
-  //     <div>emp</div>
-  //     <div>Value is {state}</div>
-  //     <div>
-  //       New value is{' '}
-  //       <input
-  //         value={savedValue}
-  //         input={(event) => {
-  //           savedValue.set(event.target!.value);
-  //         }}
-  //       ></input>
-  //     </div>
-  //     <div
-  //       click={() => {
-  //         state.set(savedValue.get());
-  //       }}
-  //     >
-  //       Save
-  //     </div>
-  //   </div>
-  // );
+  return (
+    <div>
+      <div>
+        Your name:{' '}
+        <input value={atom} input={(event) => name.set(event?.target.value)} />
+      </div>
+      <div>My name is {name}</div>
+    </div>
+  );
 };
-// // // const b: JsxNode = null as any as JsxNode2;
 
 render(document.getElementById('app')!, <App />);
