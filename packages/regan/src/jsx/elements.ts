1bb6bc3482df8e2226b8a904407396f5d360037f
utftufutukgyftryidytftuv <overby999@gmail.com>
2025-04-07 22:53:11 +0300

    07.04.25 22:53

--- packages/regan/src/jsx/elements.ts
+++ packages/regan/src/jsx/elements.ts
@@ -1,18 +1,34 @@
 type InputEventRegan = Omit<InputEvent, 'target'> & {target: HTMLInputElement};
 
 type BaseElement<TElement extends Element = any> = {
-  click?: (event: MouseEvent, element: TElement) => void;
   id?: string;
   style?: string;
   class?: string;
   className?: string;
+  title?: string;
+  hidden?: boolean;
+  tabIndex?: number;
+
+  click?: (event: MouseEvent, element: TElement) => void;
+  keydown?: (event: KeyboardEvent, element: TElement) => void;
+  keyup?: (event: KeyboardEvent, element: TElement) => void;
+  focus?: (event: FocusEvent, element: TElement) => void;
+  blur?: (event: FocusEvent, element: TElement) => void;
+  mouseover?: (event: MouseEvent, element: TElement) => void;
+  mouseout?: (event: MouseEvent, element: TElement) => void;
 } & Record<string, any>;
 
 export type Div = BaseElement & {};
 
 export type Span = BaseElement & {};
 
-export type Img = BaseElement & {};
+export type Img = BaseElement<HTMLImageElement> & {
+  src: string;
+  alt?: string;
+  width?: number | string;
+  height?: number | string;
+  loading?: 'eager' | 'lazy';
+};
 
 export type A = BaseElement & {
   href: string;
