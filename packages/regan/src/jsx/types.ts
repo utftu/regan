4e19149d51ca3eda401738dd11af59b13c9de446
utftufutukgyftryidytftuv <overby999@gmail.com>
2026-02-02 01:44:05 +0300

    02.02.26 01:44

--- packages/regan/src/jsx/types.ts
+++ packages/regan/src/jsx/types.ts
@@ -5,6 +5,10 @@ import {A, Div, Head, Html, Input, Body, Script, Template} from './elements.ts';
 export declare namespace JSX {
   export type ElementType = string | FC<any>;
   export type Element = JsxNode;
+  
+  // Пустой интерфейс = children не проверяются в props компонентов
+  export interface ElementChildrenAttribute {}
+  
   export type IntrinsicElements = {
     div: Div;
     a: A;
